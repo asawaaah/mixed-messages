1d8dfd9dc721681854eafb4387be1aebbd72536a
Gitasawaaah <asawauno@gmail.com>
2023-04-29 23:27:15 -0400

    optimized code

--- main.js
+++ main.js
@@ -4,39 +4,29 @@ const randomNumber =(num) => {
 };
 
 // Object of arrays to generate random result
-
 const moodOfTheDay = {
     breakfastFood: ['Eggs', 'Pancakes', 'Fruit Salad', 'Nutella Toast', 'Cereals with Milk'],
     foodStatus: ['Meeeh~~', 'Delicious', 'Good choice', 'Satisfying', 'not healthy'],
     activity: ['Play video games', 'Continue your lesson', 'Prepare your interview', 'Do some exercises', 'Go back to bed']
 };
 
-// store the random result 
-let yourResult = [];
+// Generate a random index for each property
+const breakfastIndex = randomNumber(moodOfTheDay.breakfastFood.length);
+const foodStatusIndex = randomNumber(moodOfTheDay.foodStatus.length);
+const activityIndex = randomNumber(moodOfTheDay.activity.length);
 
-// Iterate over the object to generate a random resultt with one resultat for each of the 3 properties 
- for (let prop in moodOfTheDay) {
-    let randomIndex = randomNumber(moodOfTheDay[prop].length)
+// Store the random result 
+const yourResult = [
+    `Today you should eat ${moodOfTheDay.breakfastFood[breakfastIndex]} for breakfast`,
+    `My rating for this breakfast is ${moodOfTheDay.foodStatus[foodStatusIndex]}`,
+    `Here is the program I suggest after you finish your breakfast: ${moodOfTheDay.activity[activityIndex]}`
+];
 
-// customize mssage using conditions
-if (prop === 'breakfastFood') {
-    yourResult.push(`Today you should eat ${moodOfTheDay.breakfastFood[randomIndex]} for breakfast`)
-}
-if (prop === 'foodStatus') {
-    yourResult.push(`My rating for this breakfast is ${moodOfTheDay.foodStatus[randomIndex]}`)
-}
-if (prop === 'activity') {
-    yourResult.push(`He is the program I suggest after you finish your breakfast: ${moodOfTheDay.activity[randomIndex]}`)
-}
-
- };
-
-
- // format the array 'yourResult" so it is readable on 3 separate lines
+// Format the array 'yourResult" so it is readable on 3 separate lines
 const formattedResult = generatedMessage => {
     formatted = yourResult.join('\n');
     console.log(formatted);
-}
+};
 
- // Call the message genetor
- formattedResult();
+// Call the message generator
+formattedResult();
